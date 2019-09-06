115f6fc5adbd7bdc7432e1b4ddbcaf93de97bfd4
Laharnar <marko.laharnar@gmail.com>
2019-09-06 19:45:28 +0200

    Added ui load between scenes

---
+++ MothershipHell3/Assets/Scripts/GlobSceneLoading.cs
@@ -0,0 +1,30 @@
+﻿/* Created by: Marko Laharnar, 6.9.2019
+ * 
+ * Purpose: 
+ * Allows any script to load a scene.
+ * + Supports UI, like button events.
+ * 
+ * Setup:
+ * Put a single instance in the scene. (Multiple instances are allowed)
+ * 
+ * 
+ * */
+
+using UnityEngine;
+
+/// <summary>
+/// Allows any script to load a scene.
+/// </summary>
+public class GlobSceneLoading : MonoBehaviour
+{
+    public void UILoadScene(string sceneName)
+    {
+        LoadScene(sceneName);
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        if (sceneName == null) return;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
