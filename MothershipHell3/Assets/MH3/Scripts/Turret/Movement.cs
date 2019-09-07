08c16060cd89deba48906f3f9de774b674027ca7
Laharnar <marko.laharnar@gmail.com>
2019-09-08 00:13:04 +0200

    update split projectile into projectile and movement

---
+++ MothershipHell3/Assets/MH3/Scripts/Turret/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+/// <summary>
+/// Directional movement with rigidbody.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour {
+    [SerializeField] Vector2 direction = Vector2.up;
+    [SerializeField] float speed = 10f;
+    Rigidbody2D rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rig.MovePosition(rig.position + (Vector2)transform.InverseTransformDirection(direction) * Time.fixedDeltaTime * speed);
+    }
+
+    internal void FlyForward()
+    {
+        direction = Vector2.up;
+    }
+}
--- MothershipHell3/Assets/MH3/Scripts/Turret/Projectile.cs
+++ MothershipHell3/Assets/MH3/Scripts/Turret/Projectile.cs
@@ -8,23 +8,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] Vector2 direction = Vector2.up;
-    [SerializeField] float speed = 10f;
-    Rigidbody2D rig;
-
     // on collision data.
     public int dealDamage = 1;
-
-    private void Awake()
-    {
-        rig = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rig.MovePosition(rig.position + (Vector2)transform.InverseTransformDirection(direction)*Time.fixedDeltaTime*speed);
-    }
 
     public void DealDamageToStats(Stats hp)
     {
