0c8086e157ce5b0291df97e022abb75d234bb2f3
Laharnar <marko.laharnar@gmail.com>
2019-10-14 21:12:47 +0200

    add[OBJECTIVES]: gui for objectives, fadeout/in system

---
+++ MothershipHell3/Assets/MH3_attempt2/Scripts/Objectives/FadeOutColor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOutColor : MonoBehaviour
+{
+    [SerializeField] Color fadeOutColor;
+    [SerializeField] Color fadeInColor;
+
+    [SerializeField] Image img;
+    [SerializeField] TMPro.TextMeshProUGUI txt;
+
+    [SerializeField] bool fadeInAtStart = false;
+
+    Color c;
+
+    private void Start()
+    {
+        if (fadeInAtStart)
+            FadeIn();
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(cFadeOut());
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(cFadeIn());
+    }
+
+    internal IEnumerator cFadeOut()
+    {
+        Debug.Log("fade ut2");
+        SetColor(fadeInColor);
+        float t = 0f;
+        while (t<1f)
+        {
+            SetColor( Color.Lerp(fadeInColor, fadeOutColor, t));
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    internal IEnumerator cFadeIn()
+    {
+        SetColor(fadeOutColor);
+        float t = 0f;
+        while (t < 1f)
+        {
+            SetColor( Color.Lerp(fadeOutColor, fadeInColor, t));
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    void SetColor(Color col)
+    {
+        if (txt != null)
+        {
+            txt.color = col;
+        }
+        if (img != null)
+        {
+            img.color = col;
+        }
+    }
+}
---
+++ MothershipHell3/Assets/MH3_attempt2/Scripts/Objectives/GUIObjectives.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIObjectives : MonoBehaviour
+{
+    [SerializeField] FadeOutColor onGameDoneFadeout;
+    [SerializeField] List<FadeOutColor> objectivesDone = new List<FadeOutColor>();
+
+    private void Start()
+    {
+        // dev test
+        Invoke("test", 3);
+    }
+    void test()
+    {
+        UIForCompleteObjective(0);
+
+    }
+
+    public void UIForCompleteMission()
+    {
+        OnDone();
+    }
+
+    public void UIForCompleteObjective(int i)
+    {
+        Debug.Log("fade ut");
+        // .
+        objectivesDone[i].FadeOut();
+    }
+
+    public void UIForFailedObjective(int i)
+    {
+        Debug.Log("Failed objective");
+    }
+
+    public void RegisterObjective()
+    {
+        // .
+    }
+
+    public void UnRegisterObjective()
+    {
+        // .
+    }
+
+    IEnumerator OnDone()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(onGameDoneFadeout.cFadeOut());
+        }
+    }
+}
